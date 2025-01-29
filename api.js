d1bf38f713b192ac3af15335d35a5b1d40fe9463
Lucifurge <tasogaresaya@gmail.com>
2025-01-29 11:16:10 +0800

    Update api.js

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // Supabase Configuration
 const SUPABASE_URL = 'https://ocdcqlcqeqrizxbvfiwp.supabase.co'; // Replace with your Supabase URL
-const SUPABASE_KEY = 'your_supabase_key'; // Replace with your Supabase Key
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jZGNxbGNxZXFyaXp4YnZmaXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc1MzkwOTEsImV4cCI6MjA1MzExNTA5MX0.g9rGkVFMxI8iqBNtGzeDvkDGfbmSZhq7J32LITaTkq0'; // Replace with your Supabase Key
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const app = express();
